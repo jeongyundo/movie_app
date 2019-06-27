68d24f7b8f79abc1ebeb42d8c0d157be70437064
정윤도 <truenorthj@gmail.com>
2019-06-28 01:07:11 +0900

    Updates

---
+++ precache-manifest.7ebaf9d87d37920f8d3c9256ca19b10a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31acf47c639c065e211dc23cf892ade6",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "660683fe8d33225a62d9",
+    "url": "/movie_app/static/css/main.602e9502.chunk.css"
+  },
+  {
+    "revision": "e083292f352b8a48b197",
+    "url": "/movie_app/static/js/2.61ebbd48.chunk.js"
+  },
+  {
+    "revision": "660683fe8d33225a62d9",
+    "url": "/movie_app/static/js/main.defe75e2.chunk.js"
+  },
+  {
+    "revision": "c6514544d436775abdc2",
+    "url": "/movie_app/static/js/runtime~main.175dda75.js"
+  }
+]);
